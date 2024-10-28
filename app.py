4a18b69c5cc72415d012fe6affc715f4f5a5b12a
Ravio <rioten353@gmail.com>
2024-10-28 10:30:43 +0000

    fire bullet

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 import sys
 import pygame
 
+from bullets.bullet import Bullet
 from player.ship import Ship
 from setting import Setting
 
@@ -19,6 +20,7 @@ class Alien:
         self.bg_color = self.setting.bg_color
 
         self.ship = Ship(self)
+        self.bullets = pygame.sprite.Group()
 
 
 
@@ -26,6 +28,8 @@ class Alien:
         while True:
             self._check_events()
             self.ship.update()
+            self.bullets.update()
+            self._remove_old_bullet()
             self._update_screen()
 
     def _check_events(self):
@@ -40,6 +44,7 @@ class Alien:
     def _update_screen(self):
         self.screen.fill(self.bg_color)
         self.ship.blit()
+        self._draw_bullets()
         pygame.display.flip()
 
     def _check_key_down_event(self, event):
@@ -49,6 +54,8 @@ class Alien:
             self.ship.ship_moving_left = True
         elif event.key == pygame.K_q:
             sys.exit()
+        elif event.key == pygame.K_SPACE:
+            self._fire_bullts()
 
     def _check_uo_down_event(self, event):
         if event.key == pygame.K_RIGHT:
@@ -60,4 +67,18 @@ class Alien:
     def _quit():
         pygame.quit()
         sys.exit()
+
+    def _fire_bullts(self):
+        if len(self.bullets) < self.setting.bullet_allow:
+            new_bullet = Bullet(self)
+            self.bullets.add(new_bullet)
+
+    def _remove_old_bullet(self):
+        for bullet in self.bullets.copy():
+            if bullet.rect.bottom <= 0:
+                self.bullets.remove(bullet)
+
+    def _draw_bullets(self):
+        for bullet in self.bullets.sprites():
+            bullet.draw_bullet()
 
--- setting.py
+++ setting.py
@@ -7,3 +7,9 @@ class Setting:
 
         self.ship_speed = 3
 
+        # bullet setting
+        self.bullet_speed = 1.0
+        self.bullet_width = 3
+        self.bullet_height = 15
+        self.bullet_color = (60, 60, 60)
+        self.bullet_allow = 3
