241c881b8a846ae513472e205958dbf79024de35
Ravio <rioten353@gmail.com>
2024-10-28 09:41:16 +0000

    initial setup and refacture

---
+++ app.py
@@ -0,0 +1,61 @@
+import sys
+import pygame
+
+from player.ship import Ship
+from setting import Setting
+
+
+class Alien:
+    def __init__(self):
+        pygame.init()
+
+        self.setting = Setting()
+
+        pygame.display.set_caption(self.setting.title)
+        self.screen = pygame.display.set_mode((self.setting.screen_width, self.setting.screen_height))
+        self.bg_color = self.setting.bg_color
+
+        self.ship = Ship(self)
+
+
+
+    def run(self):
+        while True:
+            self._check_events()
+            self.ship.update()
+            self._update_screen()
+
+    def _check_events(self):
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                self._quit()
+            elif event.type == pygame.KEYDOWN:
+                self._check_key_down_event(event)
+            elif event.type == pygame.KEYUP:
+                self._check_uo_down_event(event)
+
+    def _update_screen(self):
+        self.screen.fill(self.bg_color)
+        self.ship.blit()
+        pygame.display.flip()
+
+    def _check_key_down_event(self, event):
+        if event.key == pygame.K_RIGHT:
+            self.ship.ship_moving_right = True
+        elif event.key == pygame.K_LEFT:
+            self.ship.ship_moving_left = True
+
+    def _check_uo_down_event(self, event):
+        if event.key == pygame.K_RIGHT:
+            self.ship.ship_moving_right = False
+        elif event.key == pygame.K_LEFT:
+            self.ship.ship_moving_left = False
+
+    def _quit(self):
+        pygame.quit()
+        sys.exit()
+
+
+if __name__ == '__main__':
+    alien = Alien()
+    alien.run()
---
+++ player/ship.py
@@ -0,0 +1,30 @@
+import pygame
+
+
+class Ship:
+    def __init__(self, ai_game):
+        self.screen = ai_game.screen
+        self.setting = ai_game.setting
+        self.screen_rect = ai_game.screen.get_rect()
+
+        self.ship_image = pygame.image.load('images/player.gif')
+        self.ship_image_rect = self.ship_image.get_rect()
+
+        self.ship_image_rect.midbottom = self.screen_rect.midbottom
+
+        self.ship_moving_right = False
+        self.ship_moving_left = False
+
+        self.x = float(self.ship_image_rect.x)
+
+    def update(self):
+        if self.ship_moving_right and self.screen_rect.right > self.ship_image_rect.right:
+            self.x += self.setting.ship_speed
+
+        if self.ship_moving_left and self.screen_rect.left < self.ship_image_rect.left:
+            self.x -= self.setting.ship_speed
+
+        self.ship_image_rect.x = self.x
+
+    def blit(self):
+        self.screen.blit(self.ship_image, self.ship_image_rect)
